ef59a4ac2db078ec3f59b597949015f38a916178
datahell <elxis4@gmail.com>
2016-07-03 17:35:47 +0300

    Update DateTimePicker-i18n.js
    Added Hellenic (Greek) language

--- dist/i18n/DateTimePicker-i18n.js
+++ dist/i18n/DateTimePicker-i18n.js
@@ -51,6 +51,29 @@
 })(jQuery);
 
 
+/*
+    language: Hellenic
+    file: DateTimePicker-i18n-el
+    author: Ioannis Sannos (http://www.elxis.org)
+*/
+
+(function ($) {
+    $.DateTimePicker.i18n["el"] = $.extend($.DateTimePicker.i18n["el"], {
+        language: "el",
+        dateTimeFormat: "dd-MM-yyyy HH:mm",
+        dateFormat: "dd-MM-yyyy",
+        timeFormat: "HH:mm",
+        shortDayNames: ["Κυρ", "Δευ", "Τρί", "Τετ", "Πεμ", "Παρ", "Σάβ"],
+        fullDayNames: ["Κυριακή", "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκεύη", "Σάββατο"],
+        shortMonthNames: ["Ιαν", "Φεβ", "Μάρ", "Απρ", "Μάϊ", "Ιούν", "Ιούλ", "Αύγ", "Σεπ", "Οκτ", "Νοέ", "Δεκ"],
+        fullMonthNames: ["Ιανουάριος", "Φεβρουάριος", "Μάρτιος", "Απρίλιος", "Μάϊος", "Ιούνιος", "Ιούλιος", "Αύγουστος", "Σεπτέμβριος", "Οκτώβριος", "Νοέμβριος", "Δεκέμβριος"],
+        titleContentDate: "Ορίστε Ημέρα",
+        titleContentTime: "Ορίστε Ώρα",
+        titleContentDateTime: "Ορίστε Ημέρα & Ώρα",
+        setButtonContent: "Ορισμός",
+        clearButtonContent: "Εκκαθάριση"
+    });
+})(jQuery);
 
 /*
 
