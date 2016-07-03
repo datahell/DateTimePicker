841eb39f501b45b1acacb774de9ffde2d02e58d4
datahell <elxis4@gmail.com>
2016-07-03 17:41:17 +0300

    Create DateTimePicker-i18n-el.js
    Hellenic (Greek) language

---
+++ dist/i18n/DateTimePicker-i18n-el.js
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------------- 
+  jQuery DateTimePicker - Responsive flat design jQuery DateTime Picker plugin for Web & Mobile
+  Version 0.1.29
+  Copyright (c)2016 Curious Solutions LLP and Neha Kadam
+  http://curioussolutions.github.io/DateTimePicker
+  https://github.com/CuriousSolutions/DateTimePicker
+ ----------------------------------------------------------------------------- */
+/*
+	language: Hellenic
+	file: DateTimePicker-i18n-el
+	author: Ioannis Sannos (http://www.elxis.org)
+*/
+
+(function ($) {
+	$.DateTimePicker.i18n["el"] = $.extend($.DateTimePicker.i18n["el"], {
+		language: "el",
+		dateTimeFormat: "dd-MM-yyyy HH:mm",
+		dateFormat: "dd-MM-yyyy",
+		timeFormat: "HH:mm",
+		shortDayNames: ["Κυρ", "Δευ", "Τρί", "Τετ", "Πεμ", "Παρ", "Σάβ"],
+		fullDayNames: ["Κυριακή", "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκεύη", "Σάββατο"],
+		shortMonthNames: ["Ιαν", "Φεβ", "Μάρ", "Απρ", "Μάϊ", "Ιούν", "Ιούλ", "Αύγ", "Σεπ", "Οκτ", "Νοέ", "Δεκ"],
+		fullMonthNames: ["Ιανουάριος", "Φεβρουάριος", "Μάρτιος", "Απρίλιος", "Μάϊος", "Ιούνιος", "Ιούλιος", "Αύγουστος", "Σεπτέμβριος", "Οκτώβριος", "Νοέμβριος", "Δεκέμβριος"],
+		titleContentDate: "Ορίστε Ημέρα",
+		titleContentTime: "Ορίστε Ώρα",
+		titleContentDateTime: "Ορίστε Ημέρα & Ώρα",
+		setButtonContent: "Ορισμός",
+		clearButtonContent: "Εκκαθάριση"
+	});
+})(jQuery);
